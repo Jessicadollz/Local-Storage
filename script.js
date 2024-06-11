5b995fb7417f2fb34ac731a7eeef16b584740fcd
Jessicadollz <jesssicadollz@gmail.com>
2024-06-11 14:23:15 +0530

    Local Storage

---
+++ script.js
@@ -0,0 +1,16 @@
+let textcontent = document.querySelector("#textarea");
+
+function loadText() {
+  const savedText = localStorage.getItem("autosave-text");
+  if (savedText) {
+    textcontent.value = savedText;
+  }
+}
+
+function saveText() {
+  localStorage.setItem("autosave-text", textcontent.value);
+}
+
+textcontent.addEventListener("input", saveText);
+
+window.addEventListener("load", loadText);
